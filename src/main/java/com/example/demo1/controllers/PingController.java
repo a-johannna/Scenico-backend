62f17f150f8e4194b7786fead92dd0e24af3fa6b
johanna <johannna.villavicencio@gmail.com>
2025-05-29 17:19:11 +0200

    commit ping

---
+++ src/main/java/com/example/demo1/controllers/PingController.java
@@ -0,0 +1,13 @@
+package com.example.demo1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
