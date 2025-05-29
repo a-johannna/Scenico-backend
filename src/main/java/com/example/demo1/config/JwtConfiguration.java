c780b72b72c7f624c0b6a5a4c968389768e342d3
johanna <johannna.villavicencio@gmail.com>
2025-05-29 16:25:25 +0200

    commit

--- src/main/java/com/example/demo1/config/JwtConfiguration.java
+++ src/main/java/com/example/demo1/config/JwtConfiguration.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @Getter
 @Configuration
 public class JwtConfiguration {
-    @Value("${jwt.secret:UeL53jvW0BpTLmW2ePzR4mKnTz9cQxY3}")
+    @Value("${jwt.secret:w3bh00ks$3cr3t_wi7h_m0r3_than_32_chars}")
+//    @Value("${jwt.secret:clave-secreta-muy-segura-123}")
     private String jwtSecret;
 
     @Value("${jwt.expiration:86400000}") // 24 horas en milisegundos
