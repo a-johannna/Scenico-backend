39124acd25f9156051df073ca8f0aad656b6b754
johanna <johannna.villavicencio@gmail.com>
2025-05-29 17:48:56 +0200

    commit https

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -14,16 +14,13 @@ public class Demo1Application {
 		SpringApplication.run(Demo1Application.class, args);
 	}
 	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
+	public static class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+					.allowedOrigins("https://scenico-community-production.up.railway.app/")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowCredentials(true);
 		}
 	}
 }
